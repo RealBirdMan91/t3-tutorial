8b286de63c6a1faa6f4528713b89521dde17c040
RealBirdMan91 <birdman.coding.1991@gmail.com>
2024-04-28 10:34:01 +0200

    setup clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 
 import { Inter } from "next/font/google";
 import { TopNav } from "./_components/topnav";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,13 +21,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable} dark`}>
-        <div className="grid h-screen grid-rows-[auto,1fr]">
-          <TopNav />
-          <main className="overflow-y-scroll">{children}</main>
-        </div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`font-sans ${inter.variable} dark`}>
+          <div className="grid h-screen grid-rows-[auto,1fr]">
+            <TopNav />
+            <main className="overflow-y-scroll">{children}</main>
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
