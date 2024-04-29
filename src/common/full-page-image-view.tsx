14409cd98c830da835d2a28b34fc30870049e5cf
RealBirdMan91 <birdman.coding.1991@gmail.com>
2024-04-29 16:46:47 +0200

    work on upload btn

--- src/common/full-page-image-view.tsx
+++ src/common/full-page-image-view.tsx
@@ -11,7 +11,7 @@ export async function FullPageImageView(props: { photoId: string }) {
   const userInfo = await clerkClient.users.getUser(image.userId);
 
   return (
-    <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
+    <div className="flex h-full w-full min-w-0 items-center justify-center text-white">
       <div className="flex-shrink flex-grow">
         <img src={image.url} className="object-contain" alt={image.name} />
       </div>
--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -7,3 +7,5 @@ import {
    
   export const UploadButton = generateUploadButton<OurFileRouter>();
   export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+
+  export const { useUploadThing } = generateReactHelpers<OurFileRouter>();
